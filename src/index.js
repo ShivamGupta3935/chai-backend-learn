aa00594b9a750d0ca2a31fc2360b4fef06c170c8
shivam gupta <guptashivam 3935@gmail.com>
2023-12-13 20:49:09 +0530

    utils file added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 
 import dotenv from "dotenv"
 import connectDB from './db/index.js'
+import { app } from "./app.js"
 
 
 dotenv.config({
   path:"./env"
 })
 
+const port = process.env.PORT || 6000
+
 connectDB()
+.then( () => {
+  app.on("error", (err) => {
+    console.log("connecting error", err);
+    throw err
+  })
+
+  app.listen(port, () => {
+    console.log(`app is listen on port : ${port}`);
+  })
+})
+.catch( (err) => {
+  console.log("MongoDB connection failed ", err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+//high order function
+
+import { response } from "express"
+
+// const asyncHandler = (func) => async() => {}
+
+const asyncHandler = (fn) = async (req, res, next) => {
+     try {
+        await fn(req, res, next)
+     } catch (err) {
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+     }
+}
